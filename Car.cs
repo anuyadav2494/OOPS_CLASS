e1d7bcc1b008f87dbc2a2ecc46e14ae1c61d393d
Anu Yadav Kambalapally <anuyadav@usf.edu>
2020-02-04 21:00:56 -0500

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_CLASS
+{
+    class Car
+    {
+        public string color = "red";
+        public string model = "BMW";
+
+    }
+    class CarsClassMembers
+    {
+        public string color;
+        public string model;
+        public int year;
+        public void MaxSpeed()
+        {
+            Console.WriteLine("Car is going as fast as it can ---");
+        }
+
+    }
+    class CarConstructor
+    {
+        public string model;
+        public int year;
+        public  CarConstructor(string modelname, int yearofmanufacturing)
+        {
+            model = modelname;
+            year = yearofmanufacturing;
+        }
+    }
+
+    class CarProperties
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_CLASS
+{
+    class Program
+    {
+        
+
+
+            static void Main(string[] args)
+            {
+                //Multiple Objects
+                Car myobj1 = new Car();
+                Car myobj2 = new Car();
+                Console.WriteLine("Color of Car1 is : " + myobj1.color);
+                Console.WriteLine("Model of Car2 is : " + myobj2.model);
+                
+
+                //ClassMembers
+                CarsClassMembers car1 = new CarsClassMembers();
+                CarsClassMembers car2 = new CarsClassMembers();
+                car1.color = "red";
+                car1.model = "BMW";
+                car1.year = 2018;
+
+                car2.color = "blue";
+                car2.model = "Audi";
+                car2.year = 2019;
+                Console.WriteLine("--------Class Members--------");
+                car1.MaxSpeed();
+                car2.MaxSpeed();
+                
+                Console.WriteLine("Color of Car1 is : " + car1.color);
+                Console.WriteLine("Model of Car1 is : " + car1.model);
+                Console.WriteLine("Color of Car2 is : " + car2.color);
+                Console.WriteLine("Year of Car2 is : " + car2.year);
+
+
+            // Constructor
+            CarConstructor CarCon = new CarConstructor("BMW",1989);
+            Console.WriteLine("--------Constructors--------");
+            Console.WriteLine("Model of Car1 is : " + CarCon.model);
+            Console.WriteLine("Year of Car1 is : " + CarCon.year);
+
+
+            //Properties
+            CarProperties carprop = new CarProperties();
+            carprop.Name = "Benz";
+            Console.WriteLine("Name is :" + carprop.Name);
+            Console.ReadLine();
+
+            }
+        
+    }
+    
+}
+    
+
